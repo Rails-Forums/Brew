3fb114b5326e3d8195f14c0da8fd9e78c55f42c3
Thecharmander95 <charmandermaster07@gmail.com>
2022-04-09 15:54:05 -0400

    UNTESTED SCRIPT

---
+++ ubuntubrew.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+sudo apt install git curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+# Homebrew taps
+brew tap thoughtbot/formulae
+brew tap homebrew/services
+brew tap universal-ctags/universal-ctags
+brew tap heroku/brew
+# Brew install web devloment dependices
+brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+brew install gcc
+brew install git
+brew install openssl
+brew install rcm
+brew install reattach-to-user-namespace
+brew install the_silver_searcher
+brew install tmux
+brew install vim
+brew install watchman
+brew install zsh
+brew install luarocks
+brew install heroku/brew/heroku
+heroku autocomplete --refresh-cache
+brew install parity
+brew install postgresql@13
+brew services start postgresql@13
+brew install gh
+brew install libvips
+brew install libyaml
+brew install coreutils
+brew install postgres
+brew install redis
+brew services start redis
+# Ruby
+brew install rbenv
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+rbenv install 3.1.1
+rbenv global 3.1.1
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo Done! Homebrew has been successfully installed.
+echo Installing dot files
+git config --global core.excludesfile ~/.gitignore
+curl --remote-name https://raw.githubusercontent.com/Rails-Forums/Brew/main/dotfiles/.gitignore
+curl --remote-name https://raw.githubusercontent.com/Rails-Forums/Brew/main/dotfiles/.hushlogin
+rm .zshrc
+curl --remote-name https://raw.githubusercontent.com/Rails-Forums/Brew/main/dotfiles/.zshrc
+echo Done
+exit
