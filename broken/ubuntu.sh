18539eea16caf73c0a84ef1b969e5ae88103cc89
Thecharmander95 <charmandermaster07@gmail.com>
2024-01-06 20:13:01 -0500

    minor edits

--- broken/ubuntu.sh
+++ broken/ubuntu.sh
@@ -11,6 +11,7 @@ brew tap universal-ctags/universal-ctags
 brew tap heroku/brew
 
 # Brew install web devloment dependices
+brew install pcre
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 brew install gcc
 brew install git
@@ -23,7 +24,6 @@ brew install vim
 brew install watchman
 brew install zsh
 brew install node
-brew install node@14
 brew install yarn
 brew install luarocks
 brew install railwayapp/railway/railway
@@ -42,16 +42,16 @@ brew services start redis
 # Ruby
 brew install rbenv
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
-rbenv install 3.2.2
-rbenv global 3.2.2
+rbenv install 3.3.0
+rbenv global 3.3.0
 
 # Oh My Zsh
-# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # echo Installing dot files
-# git config --global core.excludesfile ~/.gitignore
-# curl --remote-name https://raw.githubusercontent.com/Rails-Forums/brew/main/dotfiles/.gitignore ~/.gitignore
-# curl --remote-name https://raw.githubusercontent.com/Rails-Forums/brew/main/dotfiles/.hushlogin ~/.hushlogin
-# rm ~/.zshrc
-# curl --remote-name https://raw.githubusercontent.com/Rails-Forums/brew/main/dotfiles/.zshrc ~/.zshrc
+git config --global core.excludesfile ~/.gitignore
+curl --remote-name https://raw.githubusercontent.com/Rails-Forums/brew/main/dotfiles/.gitignore ~/.gitignore
+curl --remote-name https://raw.githubusercontent.com/Rails-Forums/brew/main/dotfiles/.hushlogin ~/.hushlogin
+rm ~/.zshrc
+curl --remote-name https://raw.githubusercontent.com/Rails-Forums/brew/main/dotfiles/.zshrc ~/.zshrc
 echo Done
 exit
